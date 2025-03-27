4cf0f9680b86f4a50f38c485e51c21bd9fa9f179
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2025-03-27 19:48:49 +0530

    Update matrix_to_adj.py

--- matrix_to_adj.py
+++ matrix_to_adj.py
@@ -1,14 +1,15 @@
 def adjacency_matrix_to_list(matrix):
     adj_list = {}
     for i in range(len(matrix)):
-        adj_list[i] = [j for j in range(len(matrix[i])) if matrix[i][j] == 1]
+        # Only include connections to other nodes, ignoring self-loops
+        adj_list[i + 1] = [j + 1 for j in range(len(matrix[i])) if matrix[i][j] == 1 and i != j]
     return adj_list
 
-adj_matrix = [
+isConnected = [
     [1, 1, 0],
     [1, 1, 0],
     [0, 0, 1]
 ]
 
-adj_list = adjacency_matrix_to_list(adj_matrix)
+adj_list = adjacency_matrix_to_list(isConnected)
 print(adj_list)
