fcf9b100780040ec4410a28ca8a6321aae16797b
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2025-03-27 20:23:03 +0530

    Update edges_to_adj.py

--- edges_to_adj.py
+++ edges_to_adj.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+edges = [[0,1],[1,2],[2,0]]
 graph = defaultdict(list)
 for u, v in edges:
     graph[u].append(v)
     graph[v].append(u)
 
-edges = [[0,1],[1,2],[2,0]]
+print(graph)
